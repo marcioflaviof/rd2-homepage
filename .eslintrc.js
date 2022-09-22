506fdd829e98161a6d2ec9b4a9b99ba7472007fb
Marcio Flavio <mflaviof1995@gmail.com>
2022-09-22 15:40:13 -0300

    Fix desktop media to sync with original site

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,15 @@ module.exports = {
     browser: true,
     es2021: true,
   },
+<<<<<<< Updated upstream
   extends: [
     "next/core-web-vitals",
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
   ],
+=======
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "next/core-web-vitals"],
+>>>>>>> Stashed changes
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 13,
