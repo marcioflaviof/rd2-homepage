0157aa74fac1f78f24b30a6de9b46a850715e5d6
Marcio Flavio <mflaviof1995@gmail.com>
2022-10-11 18:44:16 -0300

    Recover from the git mess

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { NextPage } from "next";
 import Image from "next/image";
 import Head from "next/head";
@@ -9,9 +9,18 @@ import { Footer } from "src/concepts/Homepage/Footer";
 import { OutlawsForLife } from "src/concepts/Homepage/OutlawsForLife";
 import { SectionCards } from "src/concepts/Homepage/SectionsCards";
 import { Navbar } from "src/components/molecules/Navbar/Navbar";
+import { LoadingPage } from "src/components/organisms/LoadingPage/LoadingPage";
 
 const Home: NextPage = () => {
   const [showNav, setShowNav] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const interval = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+    return () => clearTimeout(interval);
+  }, []);
 
   return (
     <div>
@@ -21,46 +30,49 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <Header setShowNavbar={setShowNav} />
-        <Navbar setShowNavbar={setShowNav} show={showNav} />
-        <div className={styles.bg_image_container_desktop}>
-          <Image
-            alt="banner image"
-            src="/rd2/homepage/main_banner_caxhnr"
-            layout="responsive"
-            width="100"
-            height="42"
-            loading="eager"
-          />
-        </div>
+        <LoadingPage show={isLoading} />
+        <div className={isLoading ? styles.display_none : ""}>
+          <Header setShowNavbar={setShowNav} />
+          <Navbar setShowNavbar={setShowNav} show={showNav} />
+          <div className={styles.bg_image_container_desktop}>
+            <Image
+              alt="banner image"
+              src="/rd2/homepage/main_banner_caxhnr"
+              layout="responsive"
+              width="100"
+              height="42"
+              loading="eager"
+            />
+          </div>
 
-        <div className={styles.bg_image_container_mobile}>
-          <Image
-            alt="banner image"
-            src="/rd2/mobile/homepage/main_banner_ujmyeo"
-            layout="responsive"
-            width="100"
-            height="138"
-            loading="eager"
-          />
-        </div>
+          <div className={styles.bg_image_container_mobile}>
+            <Image
+              alt="banner image"
+              src="/rd2/mobile/homepage/main_banner_ujmyeo"
+              layout="responsive"
+              width="100"
+              height="138"
+              loading="eager"
+            />
+          </div>
 
-        <section className={styles.summary_container}>
-          <p className={styles.summary}>
-            America, 1899. The end of the Wild West era has begun. After a
-            robbery goes badly wrong in the western town of Blackwater, Arthur
-            Morgan and the Van der Linde gang are forced to flee. With federal
-            agents and the best bounty hunters in the nation massing on their
-            heels, the gang must rob, steal and fight their way across the
-            rugged heartland of America in order to survive. As deepening
-            internal divisions threaten to tear the gang apart, Arthur must make
-            a choice between his own ideals and loyalty to the gang who raised
-            him.
-          </p>
-        </section>
-        <SectionCards />
-        <OutlawsForLife />
-        <Footer />
+          <section className={styles.summary_container}>
+            <p className={styles.summary}>
+              America, 1899. The end of the Wild West era has begun. After a
+              robbery goes badly wrong in the western town of Blackwater, Arthur
+              Morgan and the Van der Linde gang are forced to flee. With federal
+              agents and the best bounty hunters in the nation massing on their
+              heels, the gang must rob, steal and fight their way across the
+              rugged heartland of America in order to survive. As deepening
+              internal divisions threaten to tear the gang apart, Arthur must
+              make a choice between his own ideals and loyalty to the gang who
+              raised him.
+            </p>
+          </section>
+          <SectionCards />
+          <OutlawsForLife />
+          <Footer />
+        </div>
       </main>
     </div>
   );
